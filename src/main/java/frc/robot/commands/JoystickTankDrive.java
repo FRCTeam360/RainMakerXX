a7fe61710d56b998bca72d384bdddaf0a05ed737
Joseph <josephartura@gmail.com>
2019-01-26 10:55:55 -0800

    Update JoystickTankDrive.java

--- src/main/java/frc/robot/commands/JoystickTankDrive.java
+++ src/main/java/frc/robot/commands/JoystickTankDrive.java
@@ -22,20 +22,31 @@ public class JoystickTankDrive extends Command {
 
   @Override
   protected void execute() {
-    if(Math.abs(OI.joyR.getRawAxis(1)) > .05) {
-      // Robot.driveTrain.driveR(-1 * OI.joyR.getRawAxis(1));
-      Robot.driveTrain.driveRMAX(-1 * OI.joyR.getRawAxis(1));
-    } else {
-      // Robot.driveTrain.driveR(0);
-      Robot.driveTrain.driveRMAX(0);
-    }
-    if(Math.abs(OI.joyL.getRawAxis(1)) > .05) {
-      // Robot.driveTrain.driveL(-1 * OI.joyL.getRawAxis(1));
-      Robot.driveTrain.driveLMAX(-1 * OI.joyL.getRawAxis(1));
-    } else {
-      // Robot.driveTrain.driveL(0);
-      Robot.driveTrain.driveLMAX(0);
-    }
+    // if(Math.abs(OI.joyR.getRawAxis(1)) > .05) {
+    //   // Robot.driveTrain.driveR(-1 * OI.joyR.getRawAxis(1));
+    //   Robot.driveTrain.driveRMAX(-1 * OI.joyR.getRawAxis(1));
+    // } else {
+    //   // Robot.driveTrain.driveR(0);
+    //   Robot.driveTrain.driveRMAX(0);
+    // }
+    // if(Math.abs(OI.joyL.getRawAxis(1)) > .05) {
+    //   // Robot.driveTrain.driveL(-1 * OI.joyL.getRawAxis(1));
+    //   Robot.driveTrain.driveLMAX(-1 * OI.joyL.getRawAxis(1));
+    // } else {
+    //   // Robot.driveTrain.driveL(0);
+    //   Robot.driveTrain.driveLMAX(0);
+    // }
+    
+      if(Math.abs(OI.joyOI.getRawAxis(1)) >= .01 || Math.abs(OI.joyOI.getRawAxis(1)) <= -.01){
+    		Robot.driveTrain.driveRMAX(OI.joyOI.getRawAxis(1));
+      }else{
+    		Robot.driveTrain.driveRMAX(0);
+    	}
+    	if(Math.abs(OI.joyOI.getRawAxis(5)) >= .01 || Math.abs(OI.joyOI.getRawAxis(5)) <= -.01){
+    		Robot.driveTrain.driveLMAX(OI.joyOI.getRawAxis(5));
+    	}else{
+    		Robot.driveTrain.driveLMAX(0);
+    	}
   }
 
   @Override
