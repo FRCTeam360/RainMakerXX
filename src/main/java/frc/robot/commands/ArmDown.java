f93efb73939a20899c04d5efc9d41d44aeaa3337
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2019-02-02 12:36:18 -0800

    Update ArmDown.java

--- src/main/java/frc/robot/commands/ArmDown.java
+++ src/main/java/frc/robot/commands/ArmDown.java
@@ -8,11 +8,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class ArmDown extends Command {
   public ArmDown() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.armControl);
   }
 
   // Called just before this Command runs the first time
@@ -23,6 +25,7 @@ public class ArmDown extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.armControl.articulateArm(-.5);
   }
 
   // Make this return true when this Command no longer needs to run execute()
