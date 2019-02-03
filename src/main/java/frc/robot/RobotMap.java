5f65a901a96db712580e1e5cab472f75dab2b64c
Joseph <josephartura@gmail.com>
2019-02-02 18:17:34 -0800

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,7 +7,10 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Compressor;
+
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -15,21 +18,22 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class RobotMap {
   public static PowerDistributionPanel pdp = new PowerDistributionPanel();
 
-  public static DoubleSolenoid shifter = new DoubleSolenoid(2, 3);
+  public static DoubleSolenoid shifter = new DoubleSolenoid(3, 2);
+
+  public static DoubleSolenoid hatchPanel = new DoubleSolenoid(4, 5);
 
   public static enum ShiftState {UP, DOWN, UNKNOWN};
   public static ShiftState shiftState = ShiftState.UNKNOWN;
   
   public static Compressor compressor = new Compressor(1);
 
-  // public static TalonSRX motorLeftMaster = new TalonSRX(0);
-	// public static TalonSRX motorLeftSlave = new TalonSRX(1);
-	// public static TalonSRX motorRightMaster = new TalonSRX(2);
-  // public static TalonSRX motorRightSlave = new TalonSRX(3);
-
   public static CANSparkMax left1Motor = new CANSparkMax(0, MotorType.kBrushless);
   public static CANSparkMax left2Motor = new CANSparkMax(1, MotorType.kBrushless);
   public static CANSparkMax right1Motor = new CANSparkMax(2, MotorType.kBrushless);
   public static CANSparkMax right2Motor = new CANSparkMax(3, MotorType.kBrushless);
+
+  public static TalonSRX armMotor = new TalonSRX(4);
+  public static TalonSRX wristMotor = new TalonSRX(5);
+  public static TalonSRX intakeMotor = new TalonSRX(6);
   
 }
