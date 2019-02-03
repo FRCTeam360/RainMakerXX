0c77768f2cbc2ca71aba479c7d1079850784f958
Joseph <josephartura@gmail.com>
2019-02-02 18:20:31 -0800

    Update IntakeControl.java

--- src/main/java/frc/robot/subsystems/IntakeControl.java
+++ src/main/java/frc/robot/subsystems/IntakeControl.java
@@ -1,3 +1,4 @@
+
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -7,7 +8,12 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.IntakeManual;
 
 /**
  * Add your docs here.
@@ -15,10 +21,15 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class IntakeControl extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  public void controlIntake(double armMotor){
+    // RobotMap.intakeMotor.setNeutralMode(NeutralMode.Brake);
+    RobotMap.intakeMotor.set(ControlMode.PercentOutput, armMotor);
+  }
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new IntakeManual());
   }
 }
