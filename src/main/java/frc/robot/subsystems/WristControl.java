49c78b1d2259f59dc592fa34d9ed7fc944c88023
Joey2001 <josephartura@gmail.com>
2019-02-14 15:52:12 -0800

    Revert "Update WristControl.java"
    This reverts commit bd6184313534e99c95862f288d054743eb0b35bc.

--- src/main/java/frc/robot/subsystems/WristControl.java
+++ src/main/java/frc/robot/subsystems/WristControl.java
@@ -50,6 +50,9 @@ public class WristControl extends Subsystem {
   public void wristReset(){
     RobotMap.wristMotor.set(ControlMode.PercentOutput, 0);
     RobotMap.wristMotor.setSelectedSensorPosition(Constants.wristResetPosition);
+    while(RobotMap.wristMotor.getSelectedSensorPosition() >= 0){
+      RobotMap.wristMotor.set(ControlMode.PercentOutput, -.3);
+    }
   }
 
   @Override
